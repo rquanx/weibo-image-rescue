0786472a0d21e3de4325b5d26fda14684b7e04d5
rquanx <runquant@gmail.com>
2024-07-21 01:42:46 +0800

    feat(main): opt with thread

--- main.py
+++ main.py
@@ -6,45 +6,62 @@ from args import parse_args
 from constant import base_headers, output_dir
 from cdn import load_cdn_info, update_cdn_info
 from custom_transport import request
-from utils import generate_file_name, save_file
+from utils import generate_file_name, generate_urls_of_all_qualities, save_file
+import concurrent.futures
 
 
-async def rescue_requests(url, ips, headers, pbar):
-    tasks = [request(url, headers, ip) for ip in ips]
-    results = []
-    for task in asyncio.as_completed(tasks):
-        res = await task
-        results.append(res)
-        pbar.update(1)
-    return results
+def save(result, dir, filename):
+    u = urlparse(result.url)
+    filename = generate_file_name(
+        u.path, result.url, result.headers.get("content-type"), filename
+    )
+    path = os.path.join(dir, filename)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    save_file(path, result.body)
+    print(f"Saved {result.url} to {path}")
 
 
-async def rescue(urls, ips, dir, filename=None):
+def create_tasks(urls, ips) -> list[tuple]:
+    tasks = []
+    for url in urls:
+        for ip in ips:
+            tasks.append((url, ip))
+    return tasks
+
+
+def request_task(url, ip):
+    return asyncio.run(request(url, base_headers, ip))
+
+
+def rescue(urls, ips, dir, filename=None):
     total_tasks = len(urls) * len(ips)
-    with tqdm(total=total_tasks) as pbar:
-        for url in urls:
-            u = urlparse(url)
-            tqdm.write(f"Started rescue for {url}")
-            results = await rescue_requests(url, ips, base_headers, pbar)
-            result = None
-            for res in results:
-                if res.err or res.status != 200:
-                    continue
-                result = res
+    tasks = create_tasks(urls, ips)
+    result = None
+
+    with tqdm(
+        total=total_tasks, desc="Rescuing"
+    ) as pbar, concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_task = {
+            executor.submit(request_task, url, ip): (url, ip) for url, ip in tasks
+        }
+
+        for future in concurrent.futures.as_completed(future_to_task):
+            task_result = future.result()
+            pbar.update(1)
+            if task_result and task_result.status == 200:
+                result = task_result
+                for f in future_to_task:
+                    f.cancel()
                 break
-            
-            if not result or result.status != 200:
-                tqdm.write(f"[FAILED] Unfortunately, {url} all {len(ips)} resolves failed.")
-                continue
-            
-            tqdm.write(f"[SUCCESS] {url} | {result.ip} | {len(result.body)}")
-            
-            filename = generate_file_name(u.path, url, result.headers.get("content-type"), filename)
-            path = os.path.join(dir, filename)
-            if not os.path.exists(dir):
-                os.makedirs(dir)
-            save_file(path, result.body)
-            tqdm.write(f"Saved {url} to {path}")
+        pbar.n = pbar.total
+        pbar.refresh()
+
+    if not result:
+        print(f"[FAILED] Unfortunately, all {total_tasks} resolves failed.")
+        return
+    print(f"[SUCCESS] {result.url} | {result.ip} | {len(result.body)}")
+    save(result, dir, filename)
 
 
 def main(args=None):
@@ -74,7 +91,8 @@ def main(args=None):
         return
 
     print(f"Using {len(ips)} cached resolves.")
-    asyncio.run(rescue([image_url], ips, f"./{output_dir}/"))
+    urls = generate_urls_of_all_qualities(image_url)
+    rescue(urls, ips, f"./{output_dir}/")
 
 
 if __name__ == "__main__":
