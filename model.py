631b004d7d24a52fc4d1171ebcb65a3f41df289f
rquanx <runquant@gmail.com>
2024-07-21 01:42:16 +0800

    feat(model): add url property to result

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 class Result:
-    def __init__(self, err=None, headers=None, ip=None, body=None, status=None):
+    def __init__(self, err=None, headers=None, ip=None, url = None,body=None, status=None):
         self.err = err
         self.headers = headers
         self.ip = ip
         self.body = body
         self.status = status
+        self.url = url
