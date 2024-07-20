1ea8911f327fd640874999d157b00735797bb3ae
rquanx <runquant@gmail.com>
2024-07-20 22:42:31 +0800

    fix(brotli): brotli dep

--- custom_transport.py
+++ custom_transport.py
@@ -1,6 +1,8 @@
 import httpx
 import socket
 from model import Result
+import brotli
+
 
 class CustomHTTPTransport(httpx.AsyncHTTPTransport):
     def __init__(self, ip, *args, **kwargs):
@@ -10,9 +12,18 @@ class CustomHTTPTransport(httpx.AsyncHTTPTransport):
     async def handle_async_request(self, request: httpx.Request) -> httpx.Response:
         # Override DNS resolution
         original_getaddrinfo = socket.getaddrinfo
+
         def getaddrinfo_override(*args, **kwargs):
-            return [(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP, '', (self.ip, 0))]
-        
+            return [
+                (
+                    socket.AF_INET,
+                    socket.SOCK_STREAM,
+                    socket.IPPROTO_TCP,
+                    "",
+                    (self.ip, 0),
+                )
+            ]
+
         socket.getaddrinfo = getaddrinfo_override
         try:
             return await super().handle_async_request(request)
@@ -22,13 +33,17 @@ class CustomHTTPTransport(httpx.AsyncHTTPTransport):
 
 async def request(url, headers, ip):
     transport = CustomHTTPTransport(ip)
-    async with httpx.AsyncClient(http2=True, headers=headers, timeout=15.0, transport=transport) as client:
+    async with httpx.AsyncClient(
+        http2=True, headers=headers, timeout=15.0, transport=transport
+    ) as client:
         try:
             response = await client.get(url)
-            if response.headers.get('Content-Encoding') == 'br':
+            if response.headers.get("Content-Encoding") == "br":
                 body = brotli.decompress(response.content)
             else:
                 body = response.content
-            return Result(status=response.status_code, headers=response.headers, ip=ip, body=body)
+            return Result(
+                status=response.status_code, headers=response.headers, ip=ip, body=body
+            )
         except Exception as e:
             return Result(err=e, ip=ip)
